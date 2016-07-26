b42963aa96fa5a0d4a0c47757b35300318e06abd
root <root@localhost.localdomain>
2016-07-26 22:34:31 +0300

    26/07/2016

---
+++ src/GameApplet.java
@@ -0,0 +1,7 @@
+import javax.swing.JApplet;
+public class GameApplet extends JApplet{
+	private static final long serialVersionUID = 1L;
+	public void init(){
+		setContentPane(new GamePanel());
+	}
+}
