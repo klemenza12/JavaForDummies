1d677f50ead7a815aee7bcacd32fba53d1e1a24d
root <root@localhost.localdomain>
2016-07-31 13:45:59 +0300

    31/07/2016

---
+++ src/ShowGameFrame.java
@@ -0,0 +1,9 @@
+
+public class ShowGameFrame {
+
+	public static void main(String[] args) {
+		new GameFrameInnerClass();
+		new GameFrame();
+	}
+
+}
---
+++ src/ShowMoneyFrame.java
@@ -0,0 +1,9 @@
+
+public class ShowMoneyFrame {
+
+	public static void main(String[] args) {
+		new MoneyFrame();
+
+	}
+
+}
